b03e49fa0a1d6228f32cb8ae4a50e1992d62c956
Yadab <yadab.sd2013@gmail.com>
2023-05-02 23:11:41 -0500

    expressApp setup & httpApp improved

--- class02/httpApp/index.js
+++ class02/httpApp/index.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 require("dotenv").config();
 const data = require("./data.json");
 
-function sendFile(res, fileName) {
+function sendHtmlFile(res, fileName) {
   fs.readFile(__dirname + "/public/" + fileName, function (error, buffer) {
     if (error) {
       res.writeHeader(data.httpCode.serverError);
@@ -19,34 +19,48 @@ function sendFile(res, fileName) {
   });
 }
 
+function sendJsonData(res) {
+  const jsonData = {
+    message: data.httpResponseMessage.postSuccess,
+  };
+  res.setHeader(
+    data.httpHeaderProperty.contentType,
+    data.httpHeaderProperty.jsonApplication
+  );
+  res.writeHeader(data.httpCode.success);
+  res.end(JSON.stringify(jsonData));
+}
+
+function sendNotFoundResponse(res) {
+  res.writeHeader(data.httpCode.notFound);
+  res.end(data.httpErrorMessage.notFound);
+}
+
 const requestResponse = function (req, res) {
   if (req.method === "POST") {
-    res.setHeader(
-      data.httpHeaderProperty.contentType,
-      data.httpHeaderProperty.jsonApplication
-    );
-    res.writeHeader(data.httpCode.success);
-    const data = {
-      message: data.httpResponse.postSuccessMessage,
-    };
-    res.end(JSON.stringify(data));
+    switch (req.url) {
+      case "/json":
+        sendJsonData(res);
+        break;
+      default:
+        sendNotFoundResponse(res);
+    }
   } else {
     switch (req.url) {
       case "/":
-        sendFile(res, "index.html");
+        sendHtmlFile(res, "index.html");
         break;
       case "/index.html":
-        sendFile(res, "/index.html");
+        sendHtmlFile(res, "/index.html");
         break;
       case "/page1.html":
-        sendFile(res, "page1.html");
+        sendHtmlFile(res, "page1.html");
         break;
       case "/page2.html":
-        sendFile(res, "page2.html");
+        sendHtmlFile(res, "page2.html");
         break;
       default:
-        res.writeHeader(data.httpCode.notFound);
-        res.end(data.httpErrorMessage.notFound);
+        sendNotFoundResponse(res);
     }
   }
 };
